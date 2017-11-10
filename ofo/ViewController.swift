2c0812a219f39ea8d67609888796e5345f2e7d67
jwxiaoming <904158347@qq.com>
2017-11-10 21:50:55 +0800

    push

---
+++ ofo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ofo
+//
+//  Created by jiji on 2017/10/16.
+//  Copyright © 2017年 jiji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
